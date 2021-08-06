957f0fdf2476d4d0c3bfe9cf5136fbf18b35b4e5
Zachoid <32700874+ZachOID@users.noreply.github.com>
2021-08-07 00:07:57 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,16 @@
 
 
 // export class using module.exports
+
+
+const Shape = require("Shape");
+
+class Circle extends Shape {
+    constructor(){
+        super();
+        this.calculateArea=()=>{
+            
+        }
+    }
+}
+export {Circle};
