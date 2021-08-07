039a2fde053cfc30b090dd49692945bb779d2fa9
Zachoid <32700874+ZachOID@users.noreply.github.com>
2021-08-07 23:44:24 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,19 +1,16 @@
 // import using require
-
+var msg = require('./shape.js');
 // declare class
 
 
 // export class using module.exports
-import {Shape} from ./src/components/shape.js
 
-const Shape = require("Shape");
-
-class Circle extends Shape {
-    constructor(){
-        super();
-        this.calculateArea=()=>{
-            
-        }
-    }
+module.exports= class Circle extends msg{
+ constructor(...args){
+  super(...args); 
+ }
+  calculateArea(){
+    console.log("2*pi*r"); 
+   return 10;
+  }
 }
-export {Circle};
